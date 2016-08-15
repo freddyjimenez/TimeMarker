a6f736382ec2276bf25cef7b72e4770db2fe9a4f
anikilatorbeta <freddy.jimen@gmail.com>
2016-08-15 18:25:57 -0300

    fix

--- src/TimeMarker.Server/Startup.cs
+++ src/TimeMarker.Server/Startup.cs
@@ -50,7 +50,6 @@ namespace TimeMarker.Server
         // This method gets called by the runtime. Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services.AddApplicationInsightsTelemetry(Configuration);
             if (IsDevelopment)
             {
